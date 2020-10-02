53ce7b327eb6cbdb228ddad8ff60ae3a36949787
Deepak chawla <deepakchawla35@gmail.com>
2020-10-02 09:32:16 +0530

    include cros in the api and requirements.txt file

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template, jsonify, request
 import requests
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route('/')
 def index():
